2b54710442230af0507749953f9c849c6cfc6455
Parkourkid2000 <deleona144@gmail.com>
2024-10-30 15:04:51 -0400

    completed the shapes background and responsiveness for the webpage

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const BoolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * BoolInt}px, ${y * BoolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else 
+   { 
+    document.body.classList.remove("dark-theme")}
+}
+
+
+
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
-  const success = document.querySelector(".modal__overlay--sucess");
+  const success = document.querySelector(".modal__overlay--success");
   loading.classList += " modal__overlay--visible";
   emailjs
-    .sendForm("service__id", "template__id", event.target, "user__id")
+    .sendForm(
+      "service_ty054hd",
+      "template_ykvurwr",
+      event.target,
+      "kcqGgt1yObcsjM_HS"
+    )
     .then(() => {
       loading.classList.remove("modal__overlay--visible");
-      success.classList += "modal__overlay--visible";
+      success.classList += " modal__overlay--visible";
     })
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact me directly at email@email.com"
+        "The email service is temporarily unavailable. Please contact me directly at deleona144@gmail.com"
       );
     });
 }
 
-let isModalOpen = false;
 function toggleModal() {
   if (isModalOpen) {
     isModalOpen = false;
